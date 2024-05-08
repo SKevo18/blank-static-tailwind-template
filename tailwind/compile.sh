cec0628eea2b3ec21e563e5c25977c7c0a9c2c69
Kevin Svitač <kevin@svit.ac>
2024-05-08 11:18:44 +0200

    `bun install`

--- tailwind/compile.sh
+++ tailwind/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+bun install
 bunx tailwindcss -i ../site/static/.tailwind.css -o ../site/static/compiled.css
 bunx cleancss ../site/static/compiled.css -o ../site/static/compiled.min.css
