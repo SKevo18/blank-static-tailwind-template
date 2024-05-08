6514d74a2dd736571d542b2e9304927e51157db5
Kevin Svitač <kevin@svit.ac>
2024-05-08 11:17:03 +0200

    change to bun

--- tailwind/compile.sh
+++ tailwind/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-npx tailwindcss -i ../site/static/.tailwind.css -o ../site/static/compiled.css
-npx cleancss ../site/static/compiled.css -o ../site/static/compiled.min.css
+bunx tailwindcss -i ../site/static/.tailwind.css -o ../site/static/compiled.css
+bunx cleancss ../site/static/compiled.css -o ../site/static/compiled.min.css
